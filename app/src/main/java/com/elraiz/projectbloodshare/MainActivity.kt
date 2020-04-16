80e5eedab074544674dd71be33b6793ce3c9fb26
elraiz <raizrock161@gmail.com>
2020-04-16 08:42:54 +0700

    ver 1

--- app/src/main/java/com/elraiz/projectbloodshare/MainActivity.kt
+++ app/src/main/java/com/elraiz/projectbloodshare/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.elraiz.projectbloodshare
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.btn_homelogin)
+        button.setOnClickListener{
+            val intent = Intent(this,register::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/elraiz/projectbloodshare/hampir.kt
+++ app/src/main/java/com/elraiz/projectbloodshare/hampir.kt
@@ -1,12 +1,20 @@
 package com.elraiz.projectbloodshare
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class hampir : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hampir)
+
+        val button = findViewById<Button>(R.id.btn_verify)
+        button.setOnClickListener{
+            val intent = Intent(this,verify::class.java)
+            startActivity(intent)
+        }
     }
 }
