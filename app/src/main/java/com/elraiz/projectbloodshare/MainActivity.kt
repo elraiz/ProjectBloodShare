20b8461c000ea1323252a3331f9d45e16d1fc784
elraiz <raizrock161@gmail.com>
2020-04-17 08:09:08 +0700

    ver 1

--- app/src/main/java/com/elraiz/projectbloodshare/MainActivity.kt
+++ app/src/main/java/com/elraiz/projectbloodshare/MainActivity.kt
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
+//NIM   10117163
+//Nama  Muhamad El Raiz
+//Kelas IF5
+//Rabu,15  April 2020   : Pembuatan halaman utama dan form registrasi
+//Rabu,16 April 2020    : Pembuatan halaman hampir dan verify,serta penambahan fungsi fungsi tombol
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/elraiz/projectbloodshare/register.kt
+++ app/src/main/java/com/elraiz/projectbloodshare/register.kt
@@ -3,13 +3,54 @@ package com.elraiz.projectbloodshare
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 
 class register : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val spinnerblood: Spinner = findViewById(R.id.spn_registerdarah)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.bloodtype,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinnerblood.adapter = adapter
+        }
+
+        val spinnerres: Spinner = findViewById(R.id.spn_registerresus)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.reshus,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinnerres.adapter = adapter
+        }
+
+        val spinnerkerja: Spinner = findViewById(R.id.spn_registerkerja)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.kerja,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinnerkerja.adapter = adapter
+        }
 
         val button = findViewById<Button>(R.id.btn_register)
         button.setOnClickListener{
